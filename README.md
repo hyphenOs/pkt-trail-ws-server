74ed127ab2f688d2d1906349b02753a12135c693
Mayur Borse <mayur@hyphenos.io>
2020-07-09 15:11:40 +0530

    Stream json periodically (frame relevant time)

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # webshark-backend
 Backend for streaming pcap data in node.js
 
-Run following command to start the server
+Run following commands to start the server
 
-### `yarn start` 
+1. `yarn install` to install dependencies from `package.json`
+2. `yarn start` to start the server
+3. `yarn build` to build the app for productionin `build` folder
 
 
--- server.js
+++ server.js
@@ -2,40 +2,6 @@ const WebSocket = require("ws");
 const fs = require("fs");
 const JSONStream = require("JSONStream");
 
-var records = [
-  { id: 1, name: "Terminator" },
-  { id: 2, name: "Predator" },
-  { id: 3, name: "True Lies" },
-  { id: 4, name: "Running Man" },
-  { id: 5, name: "Twins" },
-  // .... hundreds of thousands of records ....
-];
-
-// let rawData = fs.readFileSync("./data.json");
-// let jsonData = JSON.parse(rawData);
-// console.log(jsonData);
-
-// let readStream = fs.createReadStream("./user.json")
-
-// readStream.on("data",(chunk)=>{
-//   console.log("chunk",chunk,JSON.parse(chunk))
-// })
-
-// const transformStream = JSONStream.stringify();
-// const outputStream = fs.createWriteStream(__dirname + "temp.json");
-
-// transformStream.pipe(outputStream);
-
-// records.forEach(transformStream.write);
-
-// transformStream.end();
-
-// outputStream.on("finish", () => {
-//   console.log("JSON Serialization completed");
-// });
-
-let num = 0;
-
 const parseJSON = () => {
   const transformStream = JSONStream.parse("*");
   const inputStream = fs.createReadStream(__dirname + "/pcap-files/small.json");
@@ -43,69 +9,35 @@ const parseJSON = () => {
   inputStream
     .pipe(transformStream)
     .on("data", (data) => {
-      console.log("====================================");
-      console.log("Data: ", data);
-      console.log("time", data._source.layers.frame["frame.time_relative"]);
       let frame_relative_time = parseFloat(
-        data._source.layers["frame"]["frame.time_relative"]
+        data._source.layers.frame["frame.time_relative"]
       );
-      console.log("====================================");
-      // console.log(socketServer.clients)
-      // console.log(socketServer.clients);
-      // console.log(socketServer.clients.size);
-      // if (socketServer.clients.size) {
-      //   for (socketClient in socketServer.clients) {
-      //     socketClient.send(JSON.stringify(data));
-      //   }
-      // }
+
       setTimeout(() => {
         for (let client of socketServer.clients) {
           client.send(JSON.stringify(data));
         }
-      }, frame_relative_time * 1000 * 10);
+      }, frame_relative_time * 10 * 1000);
     })
     .on("end", () => {
       console.log("JSON Parsing complete");
     });
 };
 
-// outputStream.on("finish", parseJSON);
-
 const socketServer = new WebSocket.Server({ port: 3030 });
-console.log("listening on ", 3030);
-
-// var countIntervalID = null;
-// var streamIntervalID = null;
-// let streamTo = new Set();
-
-// console.log("count service started");
-// console.log("PCAP stream service started");
-// countIntervalID = setInterval(() => incrementCount(), 1 * 1000);
-
-// const incrementCount = () => (num += 1);
-
-// const streamData = (socketClient) => {
-//   socketClient.send(JSON.stringify({ count: num }));
-// };
-
-// const startStream = (socketClient) =>
-//   (streamIntervalID = setInterval(() => streamData(socketClient), 1 * 1000));
-// // parseJSON(socketClient);
-
-// const stopStream = () => clearInterval(streamIntervalID);
+console.log("listening on 3030");
 
 socketServer.on("connection", (socketClient) => {
-  console.log("connected to client:");
+  console.log("Connected to client");
   console.log("Total connected clients:", socketServer.clients.size);
 
   socketClient.on("message", (message) => {
     console.log("Message from client: ", message);
-    console.log(message === "start");
     if (message === "start") {
-      console.log("starting");
+      console.log("Start JSON streaming");
       parseJSON();
     } else {
-      console.log("stopping");
+      console.log("Stop JSON streaming (not implemented yet)");
     }
   });
 
@@ -113,5 +45,3 @@ socketServer.on("connection", (socketClient) => {
     console.log("Connection closed ");
   });
 });
-
-// clearInterval(countIntervalID);
