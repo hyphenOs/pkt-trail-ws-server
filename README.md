53c56c5dbe6fd79149918de2488d806728785b66
Abhijit Gadgil <gabhijit@hyphenos.io>
2020-07-16 17:52:28 +0530

    Supports Live Packet Capture
    - Picks up packets from a redis queue (Right now it's hard-coded)
    - Defined an Event Emitter called 'packet', so whenever there is a new
      packet available (or empty packet), we trigger the event. This causes
      next packet to be read
    - Supports start/stop and multiple captures
    - Version Reflected to v0.0.1

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # webshark-backend
-Backend for streaming pcap data in node.js
+
+Backend for streaming pcap data in node.js. Supports live capture.
+
+Note: To actually see live capture, the corresponding ['dissection' App]()
+needs to be running.
+
+## Getting Started
 
 Run following commands to start the server
 
@@ -8,3 +14,13 @@ Run following commands to start the server
 3. `yarn build` to build the app for productionin `build` folder
 
 
+## Known Issues
+
+For live capture, the data is picked from a Redis Queue and this is hard-coded
+right now. Redis configuration would change later on.
+
+Lot of things are hard-coded right now.
+We should create a wrapper server that wraps, Redis and WebSocket Server and
+should have it's own Configuration. Eventually likely we are going to need a
+lot of 'such' servers to grow with number of clients (but one per Redis
+Queue).
--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const WebSocket = require("ws");
 const fs = require("fs");
 const JSONStream = require("JSONStream");
+const redis = require("redis");
+const EventEmitter = require("events");
+const { promisify } = require("util");
 
+const redisClient = redis.createClient({db:7});
+const asyncBlpop = promisify(redisClient.blpop).bind(redisClient);
+
+class PacketStreamer extends EventEmitter {};
+
+const pktStreamer = new PacketStreamer();
+
+pktStreamer.on("packet", () => {
+
+  asyncBlpop("paket_queue:wlp2s0", 1)
+    .then( (data) => {
+        if (data) {
+          console.log(data);
+          streamPacket(data[1]);
+        }
+      pktStreamer.emit("packet");
+    })
+    .catch( (error) => {
+        console.error(error);
+    });
+});
+
+const streamPacket = (data) => {
+  for (let client of socketServer.clients) {
+    if (client.active === true) {
+      client.send(data);
+    } else {
+      console.log("client not active");
+    }
+  }
+};
+
+// No longer used, can delete later
 const parseJSON = () => {
   const transformStream = JSONStream.parse("*");
   const inputStream = fs.createReadStream(__dirname + "/pcap-files/small.json");
@@ -15,9 +51,13 @@ const parseJSON = () => {
 
       setTimeout(() => {
         for (let client of socketServer.clients) {
-          client.send(JSON.stringify(data));
+          if (client.active === true) {
+            client.send(JSON.stringify(data));
+          } else {
+            console.log("client not active");
+          }
         }
-      }, frame_relative_time * 10 * 1000);
+      }, frame_relative_time * 30 * 1000);
     })
     .on("end", () => {
       console.log("JSON Parsing complete");
@@ -27,17 +67,17 @@ const parseJSON = () => {
 const socketServer = new WebSocket.Server({ port: 3030 });
 console.log("listening on 3030");
 
+pktStreamer.emit("packet");
+
 socketServer.on("connection", (socketClient) => {
   console.log("Connected to client");
-  console.log("Total connected clients:", socketServer.clients.size);
 
   socketClient.on("message", (message) => {
-    console.log("Message from client: ", message);
     if (message === "start") {
       console.log("Start JSON streaming");
-      parseJSON();
-    } else {
-      console.log("Stop JSON streaming (not implemented yet)");
+      socketClient.active = true;
+    } else if (message === "stop") {
+      socketClient.active = false;
     }
   });
 
